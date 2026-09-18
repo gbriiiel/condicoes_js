b9a3b21291e08bd21f5a529ed38dcc36122c74e8
gbriiiel <gabivictorgol@gmail.com>
2026-09-18 09:39:29 -0300

    Add files via upload

---
+++ ex010.js
@@ -0,0 +1,7 @@
+var vel = 12.5
+console.log(`A velocidade do seu carro eh ${vel} km/h`)
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}else{
+    console.log(`dirija sempre usando cinto de segurança!`)
+}
